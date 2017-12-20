0ec2b5f27098fdc21186041df51a5f69b79f45b2
Luc Juggery <luc.juggery@gmail.com>
2017-12-20 21:21:31 +0100

    Set STORAGE_URL from value in env variable

--- backend/app/entrypoint.sh
+++ backend/app/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# TOREMOVE
-# ugly fix to change "localhost" into "redis" for communication between service
-# will be done through env / secret later on
+#TEMPORARY: fix to change "localhost" into "redis" for communication between service
 sed -i '' -e s/localhost/redis/ /srv/kernelci-backend/app/taskqueue/celeryconfig.py
+
+#TEMPORARY: fix to set the storage url
+sed -i '' -e s/STORAGE_URL/${STORAGE_URL}/ /etc/linaro/kernelci-backend.cfg
 
 # Wait until mongo is up and running
 echo "-> waiting for mongo to be available"
