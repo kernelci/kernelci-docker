04d6065ef8eacdc1e01f8409323655e08c600c10
Luc Juggery <luc.juggery@gmail.com>
2018-01-03 16:53:42 +0100

    Enhance token update in dev

--- dev-start.sh
+++ dev-start.sh
@@ -5,6 +5,7 @@ IP=$(echo $DOCKER_HOST | cut -d'/' -f3 | cut -d':' -f1)
 if [ "$IP" = "" ]; then
   IP="127.0.0.1"
 fi
+export IP
 
 ## Deploy the application
 
@@ -34,16 +35,18 @@ while [ "$TOKEN" = "" ];do
   TOKEN=$(curl -m 3 -s -X POST -H "Content-Type: application/json" -H "Authorization: MASTER_KEY" -d '{"email": "adm@kernelci.org", "admin": 1}' $IP:8081/token | docker container run --rm -i lucj/jq -r .result[0].token 2>/dev/null)
   sleep 1
 done
+echo $TOKEN > .kernelci_token
 echo "-> token returned: $TOKEN"
 
 ### Update frontend with token created
 
 sed -e s/API_TOKEN/$TOKEN/ frontend/flask_settings-TPL > frontend/flask_settings
 
-echo "-> wait will frontend is restarted"
+echo "-> wait while frontend is restarted"
 docker-compose stop frontend
 docker-compose start frontend
 
 echo "-> application configured"
-echo "--> backend available on port 8081"
 echo "--> frontend available on port 8080"
+echo "--> backend  available on port 8081"
+echo "--> storage  available on port 8082"
