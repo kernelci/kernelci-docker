e6910f7743f22d119c24c877e19ee9d9f25327c0
Loys Ollivier <lollivier@baylibre.com>
2018-01-03 10:55:58 +0100

    seed.sh: Fix mongorestore issues with dump location
    There seem to be a bug restoring a database depending on the path where
    the mongorestore command is executed.
    
    Edit the script to use a PATH and db name specific to kernel-ci. Only
    the tarball file name is and should be generic.
    
    Signed-off-by: Loys Ollivier <lollivier@baylibre.com>

--- seed.sh
+++ seed.sh
@@ -4,20 +4,28 @@ set -x
 DUMP_FILE=$1
 if [ ! -e "$DUMP_FILE" ];then
     echo "-> please provide an exiting dump file"
-    exit 0
+    exit 1
 fi
 
 ## Get db container
 ID=$(docker ps -q --filter "label=com.docker.swarm.service.name=kernelci_mongo" 2>/dev/null)
 if [ "$ID" = "" ];then
-  echo "-> cannot found container for service kernelci_mongo"
-  exit 0
+  echo "--Container for service kernelci_mongo not found exiting--"
+  exit 1
 fi
 
 ## Copy dump to container
-DUMP_FOLDER=$(tar xvf $DUMP_FILE | head -n 1)
+tar xvf $DUMP_FILE
+if [ $? -eq 0 ];then
+  echo "--Databse backup extracted correctly--"
+else
+  echo "--Something went wrong wile extracting the database from $DUMP_FILE--"
+  exit 1
+fi
+DUMP_FOLDER=$(sed -e 's/.tar.gz//' <<<$DUMP_FILE)
 docker cp $DUMP_FOLDER $ID:/tmp
 rm -r ./$DUMP_FOLDER
 
 ## Restore dump
-docker exec $ID /bin/bash -c "mongorestore -d kernel-ci /tmp/$DUMP_FOLDER "
+docker exec $ID /bin/bash -c "mongorestore --db=kernel-ci /tmp/$DUMP_FOLDER/kernel-ci"
+exit 0
