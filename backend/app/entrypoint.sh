f32082babc1ae275a6b19896ccd6fcead4a418fe
Luc Juggery <luc.juggery@gmail.com>
2018-01-04 19:58:27 +0100

    Fix storage file server

--- backend/app/entrypoint.sh
+++ backend/app/entrypoint.sh
@@ -3,9 +3,6 @@
 #TEMPORARY: fix to change "localhost" into "redis" for communication between service
 sed -i '' -e s/localhost/redis/ /srv/kernelci-backend/app/taskqueue/celeryconfig.py
 
-#TEMPORARY: fix to set the storage url
-sed -i '' -e "s@STORAGE_URL@$STORAGE_URL@" /etc/linaro/kernelci-backend.cfg
-
 # Wait until mongo is up and running
 echo "-> waiting for mongo to be available"
 mongo --host mongo --eval "db.stats()"
--- backend/celery/entrypoint.sh
+++ backend/celery/entrypoint.sh
@@ -3,9 +3,6 @@
 #TEMPORARY: fix to change "localhost" into "redis" for communication between service
 sed -i '' -e s/localhost/redis/ /home/user/kernelci-backend/app/taskqueue/celeryconfig.py
 
-#TEMPORARY: fix to set the storage url
-sed -i '' -e "s@STORAGE_URL@$STORAGE_URL@" /etc/linaro/kernelci-celery.cfg
-
 # Run celery worker
 cd /home/user/kernelci-backend/app
 celery worker -Ofair --autoscale=3,1 --app=taskqueue --broker=redis://redis:6379/0 -E
