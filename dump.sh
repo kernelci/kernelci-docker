7a4b1b9f15ba622e9969194095c6f3cb51597227
Luc Juggery <luc.juggery@gmail.com>
2018-01-25 22:40:46 +0100

    Backup logs file as required by #9

--- dump.sh
+++ dump.sh
@@ -6,6 +6,8 @@ ts=$(date -u "+%Y%m%dT%H%M%S")
 # Local dump folder
 DUMP_FOLDER="/tmp"
 
+## Backup DB
+
 # Name of the database to dump
 DB="kernel-ci"
 
@@ -17,3 +19,12 @@ docker exec $db mongodump -d $DB --archive=/tmp/kernelci-$ts.gz --gzip
 
 # Save on host (/var/lib/kernelci/dump)
 docker cp $db:/tmp/kernelci-$ts.gz ${DUMP_FOLDER:-/tmp}
+
+## Backup logs
+
+# Run alpine container using logs volume
+docker run \
+  --rm \
+  -v kernelci_kci:/tmp/logs \
+  -v ${DUMP_FOLDER:-/tmp}:/tmp/dump \
+  alpine tar -zcvf /tmp/dump/kernelci-logs-$ts.gz /tmp/logs
