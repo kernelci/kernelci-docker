6e6d44e46e2175946f6bb95d54f98b32a3a35f6a
Corentin LABBE <clabbe@baylibre.com>
2018-01-29 15:11:20 +0100

    Add a timeout to all curl actions
    In case of a failling frontend, some action could fail forever.
    Add a timeout for quiting in that case.

--- dev-start.sh
+++ dev-start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TIMEOUTMAX=120
+
 # Get host's IP (127.0.0.1 is assumed if $DOCKET_HOST is empty)
 IP=$(echo $DOCKER_HOST | cut -d'/' -f3 | cut -d':' -f1)
 if [ "$IP" = "" ]; then
@@ -21,12 +23,24 @@ docker-compose up -d || exit $?
 ## Wait for the application to be available
 
 echo "-> waiting for backend..."
+TIMEOUT=0
 while [ $(curl -s -m 3 -o /dev/null -w "%{http_code}" $IP:8081) -ne 200 ]; do
    sleep 1
+   TIMEOUT=$(($TIMEOUT+1))
+   if [ $TIMEOUT -ge $TIMEOUTMAX ];then
+       echo "TIMEOUT!"
+       exit 1
+   fi
 done
 echo "-> waiting for frontend..."
+TIMEOUT=0
 while [ $(curl -s -m 3 -o /dev/null -w "%{http_code}" $IP:8080) -ne 200 ]; do
   sleep 1
+   TIMEOUT=$(($TIMEOUT+1))
+   if [ $TIMEOUT -ge $TIMEOUTMAX ];then
+       echo "TIMEOUT!"
+       exit 1
+   fi
 done
 
 ## Configure the application
@@ -37,9 +51,15 @@ echo "-> configuring the application..."
 
 echo "-> requesting token from backend..."
 TOKEN=""
+TIMEOUT=0
 while [ "$TOKEN" = "" ];do
   TOKEN=$(curl -m 3 -s -X POST -H "Content-Type: application/json" -H "Authorization: MASTER_KEY" -d '{"email": "adm@kernelci.org", "admin": 1}' $IP:8081/token | docker container run --rm -i lucj/jq -r .result[0].token 2>/dev/null)
   sleep 1
+   TIMEOUT=$(($TIMEOUT+1))
+   if [ $TIMEOUT -ge 60 ];then
+       echo "TIMEOUT!"
+       exit 1
+   fi
 done
 echo $TOKEN > .kernelci_token
 echo "-> token returned: $TOKEN"
